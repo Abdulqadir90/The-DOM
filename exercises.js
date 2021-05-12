ea6d2bbf507c8f83ba15c6e82dd855041e7869e0
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-05-12 19:34:24 +0100

    insert row function

--- exercises.js
+++ exercises.js
@@ -61,20 +61,49 @@
 // Write a JavaScript function to get the value of the href, hreflang, rel, target, 
 // and type attributes of the specified link. 
 
-function attributes() 
+// function attributes() 
+// {
+//     let a = document.getElementsById.href;
+//     alert('value of href attribute: '+a );
+    
+//     let b = document.getElementsById.hreflang;
+//     alert('value of hreflang attribute: '+b );
+    
+//     let c = document.getElementsById.rel;
+//     alert('value of rel attribute: '+c );
+    
+//     let d = document.getElementsById.target;
+//     alert('value of target attribute: '+d);
+    
+//     let e = document.getElementsById.type;
+//     alert('value of type attribute: '+e );
+// }
+
+// 5. Write a JavaScript function to add rows to a table. Go to the editor
+// Sample HTML file :
+
+// <!DOCTYPE html>
+// <html><head>
+// <meta charset=utf-8 />
+// <title>Insert row in a table - w3resource</title>
+// </head><body>
+// <table id="sampleTable" border="1">
+// <tr><td>Row1 cell1</td>
+// <td>Row1 cell2</td></tr>
+// <tr><td>Row2 cell1</td>
+// <td>Row2 cell2</td></tr>
+// </table><br>
+// <input type="button" onclick="insert_Row()" value="Insert row"> 
+// </body></html>
+
+function insert_row()
 {
-    let a = document.getElementsById.href;
-    alert('value of href attribute: '+a );
-    
-    let b = document.getElementsById.hreflang;
-    alert('value of hreflang attribute: '+b );
-    
-    let c = document.getElementsById.rel;
-    alert('value of rel attribute: '+c );
-    
-    let d = document.getElementsById.target;
-    alert('value of target attribute: '+d);
-    
-    let e = document.getElementsById.type;
-    alert('value of type attribute: '+e );
+let table = document.getElementsById("sampleTable");
+let row = table.insert_row(0);
+let one = row.insertCell(0)
+let two = row.insert_row(1)
+
+one.innerHTML = "new cell1";
+two.innerHTML = "newcell2";
+
 }
